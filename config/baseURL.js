8cea0ae54232193793b65f6dc94c412a78e1053a
parasgarg.github <parasgarg.github@gmail.com>
2021-10-04 12:42:40 +0530

    minor bug fix

--- config/baseURL.js
+++ config/baseURL.js
@@ -2,7 +2,11 @@ export default [
   "authProviderProvider",
   "dbProviderProvider",
   function (authProviderProvider, dbProviderProvider) {
-    authProviderProvider.setBaseURL("https://contacts-server-pg.herokuapp.com/api");
-    dbProviderProvider.setBaseURL("https://contacts-server-pg.herokuapp.com/api");
+    authProviderProvider.setBaseURL(
+      "https://contacts-server-pg.herokuapp.com/api"
+    );
+    dbProviderProvider.setBaseURL(
+      "https://contacts-server-pg.herokuapp.com/api"
+    );
   },
 ];
